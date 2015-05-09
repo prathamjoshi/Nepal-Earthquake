de2c6df71268482be0e8490bfd9a815a2341bc7e
Pratham Joshi <pjoshi@bennington.edu>
2015-05-09 16:16:09 -0400

    world map popup formatting initial

--- js/world-map.js
+++ js/world-map.js
@@ -1,7 +1,6 @@
 
 var world_map = new Datamap({
 					element: document.getElementById('world-map'),
-					/*
 					fills: {
 								defaultFill : 'darkgrey',
 								HIGH: 'red',
@@ -11,13 +10,27 @@ var world_map = new Datamap({
 					data: {
 								USA: {
 									fillKey : 'MEDIUM',
-									aid: 20
+									monetary: 20,
+									personnel: 2
 								}
 					},
 					geographyConfig: {
 						highlightOnHover: true,
-            			popupOnHover: true
+            			popupOnHover: true,
+            			popupTemplate: function(geo, data) {
+            				if (data) {
+            					var message = "<div class=\"hoverinfo\"><strong>" + geo.properties.name + "</strong><br>";
+		                    	message += "<br> <b>Aid</b>: <br>";
+		                    	message += "<b>Monetary</b>: <i>" + data.monetary + "</i><br>";
+		                    	message += "<b>Personnel</b>: <i>" + data.personnel + "</i><br>";
+		                    	message += "</div>";
+		                    	return [message].join('');
+		                    } else {
+		                    	var message = "<div class=\"hoverinfo\"><strong>" + geo.properties.name + "</strong>"
+		                    	message += "<br> <b>Aid</b>: <i>data not available</i>";
+		                    	message += "</div>";
+		                    	return [message].join('');
+		                    }
+            			}
         			}
-        			*/
-
 				});
