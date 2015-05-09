c88778b3b84cb9dd93ef81b2ca2c6029e6c98c3b
Pratham Joshi <pjoshi@bennington.edu>
2015-05-09 16:03:39 -0400

    fixed popup placing for world maps

--- js/world-map.js
+++ js/world-map.js
@@ -1,2 +1,23 @@
 
-var world_map = new Datamap({element: document.getElementById('world-map')});
+var world_map = new Datamap({
+					element: document.getElementById('world-map'),
+					/*
+					fills: {
+								defaultFill : 'darkgrey',
+								HIGH: 'red',
+								LOW: 'orange',
+								MEDIUM: 'blue'
+					},
+					data: {
+								USA: {
+									fillKey : 'MEDIUM',
+									aid: 20
+								}
+					},
+					geographyConfig: {
+						highlightOnHover: true,
+            			popupOnHover: true
+        			}
+        			*/
+
+				});
