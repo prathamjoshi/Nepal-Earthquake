cf819d3694c79336d59101a8d4452fa2d6fad3d8
Pratham Joshi <pjoshi@bennington.edu>
2015-05-10 15:14:30 -0400

    added footer

--- js/comparison.js
+++ js/comparison.js
@@ -14,7 +14,15 @@
         myChart.addSeries(["Date", "Location"], dimple.plot.bubble);
         //myChart.addLegend(200, 10, 360, 20, "right");
         myChart.draw();
+
+        comparison_svg.append("text")
+           .attr("x", myChart._xPixels() + myChart._widthPixels() / 2)
+           .attr("y", myChart._yPixels() - 10)
+           .style("text-anchor", "middle")
+           .style("font-family", "sans-serif")
+           .text("10,000 fatalities and below:");
       });
+
 
  var comparison_svg2 = dimple.newSvg("#comparison-chart2", 590, 400);
       d3.tsv("/data/deadliest_quakes.tsv", function (data) {
@@ -27,9 +35,15 @@
         y_axis.overrideMin = 5;
         y_axis.overrideMax = 9.5;
         x_axis.overrideMin = 0;
-        x_axis.overrideMax = 100000;
+        x_axis.overrideMax = 100000 ;
         
         myChart.addSeries(["Date", "Location"], dimple.plot.bubble);
-        //myChart.addLegend(200, 10, 360, 20, "right");
         myChart.draw();
+
+        comparison_svg2.append("text")
+           .attr("x", myChart._xPixels() + myChart._widthPixels() / 2)
+           .attr("y", myChart._yPixels() - 10)
+           .style("text-anchor", "middle")
+           .style("font-family", "sans-serif")
+           .text("8,000 fatalities and above:");
       });
