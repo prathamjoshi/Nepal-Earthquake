0ef26203c87736bbeb86839ee2987cb0ea5c96d1
Pratham Joshi <pjoshi@bennington.edu>
2015-05-10 14:15:08 -0400

    added second comparison chart

--- js/comparison.js
+++ js/comparison.js
@@ -5,10 +5,30 @@
         myChart.setBounds(60, 30, 500, 330)
         var x_axis = myChart.addMeasureAxis("x", "Deaths");
         var y_axis = myChart.addMeasureAxis("y", "Magnitude");
+
         y_axis.overrideMin = 5;
         y_axis.overrideMax = 9.5;
         x_axis.overrideMin = 0;
         x_axis.overrideMax = 10000;
+
+        myChart.addSeries(["Date", "Location"], dimple.plot.bubble);
+        //myChart.addLegend(200, 10, 360, 20, "right");
+        myChart.draw();
+      });
+
+ var comparison_svg2 = dimple.newSvg("#comparison-chart2", 590, 400);
+      d3.tsv("/data/deadliest_quakes.tsv", function (data) {
+        //data = dimple.filterData(data, "Date", "01/12/2012");
+        var myChart = new dimple.chart(comparison_svg2, data);
+        myChart.setBounds(60, 30, 500, 330)
+        var x_axis = myChart.addMeasureAxis("x", "Deaths");
+        var y_axis = myChart.addMeasureAxis("y", "Magnitude");
+        
+        y_axis.overrideMin = 5;
+        y_axis.overrideMax = 9.5;
+        x_axis.overrideMin = 0;
+        x_axis.overrideMax = 100000;
+        
         myChart.addSeries(["Date", "Location"], dimple.plot.bubble);
         //myChart.addLegend(200, 10, 360, 20, "right");
         myChart.draw();
